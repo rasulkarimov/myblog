f4d65fc5a00f50659c7c83a6418a91d8b544af21
rasulkarimov <rasul.karimov@gmail.com>
2021-02-06 20:44:54 +0300

    first commit

---
+++ app/init_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second post', 'Content for the second content')
+            )
+
+connection.commit()
+connection.close
